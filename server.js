42646a904879d5ff82bb315fa9dd32cfb8c6369c
Malek184 <malek.benkhedher@esprit.tn>
2024-02-05 13:36:17 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http= require('http')
+
+const server= http.createServer((req,res)=>{
+   
+    res.write(req.url)
+
+    res.end()
+})
+
+server.listen(5000,()=>{console.log('server running port 5000')})
+
